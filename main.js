368798e6358618fe18fe11bf1f4e58cfe7342d5d
jose carlos <castillo.villa@nube.unadmexico.mx>
2020-09-28 00:53:54 -0500

    se realizaron funciiones

--- main.js
+++ main.js
@@ -1,30 +1,59 @@
-const productos = [
-    {
-        nombre: "Hamburguesa simple",
-        costo: 20,
-        codigo:"p1001"
-    },
-    {
-        nombre: "Hamburguesa doble",
-        costo: 30,
-        codigo:"p1002"
-    },
-    {
-        nombre: "papas fritas",
-        costo: 8,
-        codigo:"p1003"
-    },
+const usuario ={
+    nombre:"carlos",
+    edad: 30,
+    deuda: 0
+}
 
-    {
-        nombre: "refresco personal",
-        costo: 6,
-        codigo:"p1004"
-    }
+let pedido =[]
+let costoPedido = 0
 
-]
 const mostrarMenu = () => {
     console.log('CODIGO -  NOMBRE PRODUCTO - PRECIO')
     productos.forEach(producto => console.log(producto.codigo+' - '+producto.nombre+' - $'+producto.costo))
 }
 
-console.log(mostrarMenu())
+const pedirProducto = cod => {
+    if (!cod) return 'Ingrese un codigo valido.'
+
+   const productoEncontrado =  productos.find(producto => producto.codigo === cod)
+   if (!productoEncontrado) return 'El producto no existe'
+
+   pedido.push(productoEncontrado)
+   console.log('El produco se agregado. el pedido es:')
+   return verPedido()
+    
+}
+
+const verPedido = () => pedido
+
+const calcularCosto = () =>{
+    let costo = 0
+    for(producto of pedido){
+        costo += producto.costo
+    }
+    costoPedido = costo
+    return costoPedido
+}
+
+const finalizarPedido = () =>{
+    calcularCosto()
+    usuario.deuda = costoPedido
+    pedido = []
+    costoPedido = 0
+    return usuario.nombre +' debes de pagar $ '+usuario.deuda+ ' dolares'
+}
+
+const pagarPedido = montoEntregado =>{
+    
+    if (montoEntregado < usuario.deuda){
+        return 'es necesario mas dinero.'
+    }else if(montoEntregado === usuario.deuda){
+        usuario.deuda = 0
+        return 'tu pedido ha sido pagado'
+    }else{
+        usuario.deuda = 0
+        resta = parseFloat(montoEntregado) - parseFloat(usuario.deuda)
+        return 'Pedido pagado el cambio es: $'+ resta
+    }
+}
+
---
+++ productos.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+        nombre: "Hamburguesa simple",
+        costo: 20,
+        codigo:"p1001"
+    },
+    {
+        nombre: "Hamburguesa doble",
+        costo: 30,
+        codigo:"p1002"
+    },
+    {
+        nombre: "papas fritas",
+        costo: 8,
+        codigo:"p1003"
+    },
+
+    {
+        nombre: "refresco personal",
+        costo: 6,
+        codigo:"p1004"
+    }
+
+]
